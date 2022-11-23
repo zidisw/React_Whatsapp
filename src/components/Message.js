0c7ccc167231423f5d1302b1517b3190e96daebe
Akram <mantavbang@gmail.com>
2022-11-24 00:41:06 +0800

    Message Components

--- src/components/Message.js
+++ src/components/Message.js
@@ -2,17 +2,17 @@ import React from "react";
 
 const Message = ({ css1, css2, name, message, timestamp }) => {
   return (
-    <div className={`${css2} flex`}>
+    <div className={`${css2} chatDisplay`}>
       <div
-        className={`${css1} flex flex-col p-2 pr-16 pl-4 m-2 lg:mx-6 rounded-[1rem]  `}
+        className={`${css1} chatText  `}
       >
-        <div className="flex flex-col ">
-          {name && <p className="pr-3 font-bold">{name}</p>}
+        <div className="inChat ">
+          {name && <p className="nameChat">{name}</p>}
 
-          <p className="text-[15px] -mt-1">{message}</p>
+          <p className="textInChat">{message}</p>
         </div>
 
-        <p className="text-[12px] text-gray-500 text-end -mr-14 -my-1.5">
+        <p className="timeChat">
           {timestamp}
         </p>
       </div>
