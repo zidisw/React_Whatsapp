9f8f81ca0bb30b1ac167bb12806f210861d70385
Zulfiqry Saadputra <zsp301@gmail.com>
2022-11-23 16:59:25 +0800

    navbar component styling to index.css

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,13 +5,13 @@ import { HiOutlineMenu } from "react-icons/hi";
 
 const Navbar = () => {
   return (
-    <div className="flex items-center justify-between px-4 pt-2 pb-2 bg-[#eeedef] h-[4.5rem] w-screen md:w-full md:rounded-tl-[2rem]">
-      <div className="w-12  h-12 flex ">
-        <img src={profile} alt="" className="object-cover  rounded-full" />
+    <div className="navBar">
+      <div className="profilePic">
+        <img src={profile} alt="" className="profilePicImage" />
       </div>
-      <div className="flex items-center gap-6">
-        <BiMessage className="w-6 h-6" />
-        <HiOutlineMenu className="w-6 h-6" />
+      <div className="navBarIcons">
+        <BiMessage className="navBarIcon" />
+        <HiOutlineMenu className="navBarIcon" />
       </div>
     </div>
   );
