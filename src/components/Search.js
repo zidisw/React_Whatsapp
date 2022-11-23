d75c6b176f39aa4f1f176ef96ff02cb79caa4417
Zulfiqry Saadputra <zsp301@gmail.com>
2022-11-23 16:19:25 +0800

    search component styling to index.css

--- src/components/Search.js
+++ src/components/Search.js
@@ -3,15 +3,14 @@ import { FiSearch } from "react-icons/fi";
 
 const Search = () => {
   return (
-    <div className="relative flex items-center  bg-[#fafafa] py-2 pt-1">
-      <div className="absolute left-6 pl-1">
+    <div className="searchBar">
+      <div className="searchLogo">
         <FiSearch />
       </div>
       <input
         type="search"
         placeholder="Search for friends"
-        className="w-full pl-9  m-2 border   
-mx-4 outline-0 rounded-[1rem] p-1"
+        className="searchInput"
       />
     </div>
   );
