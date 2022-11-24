3684d001a798c7841e80f2ff24a84867a06d073f
vierylatumeten02 <112059210+vierylatumeten02@users.noreply.github.com>
2022-11-24 14:42:06 +0800

    update Chat
    modifikasi isi chat & className

--- src/components/Chats.js
+++ src/components/Chats.js
@@ -1,7 +1,6 @@
 import React from "react";
-import profile3 from "../assets/face3.jpg";
-import landscape from "../assets/landscape.jpg";
-import mountains from "../assets/mountains.jpg";
+import profil_chat from "../assets/nayeon_profil.jpg";
+import chat_background from "../assets/chat_background.jpg";
 import { TbApps } from "react-icons/tb";
 import { AiOutlineFile } from "react-icons/ai";
 import { GrSettingsOption } from "react-icons/gr";
@@ -10,62 +9,60 @@ import Message from "../components/Message";
 const Chats = () => {
   const messages = [
     {
-      name: "Joe Doe",
-      message: "Hi i'm Joe Doe",
+      name: "Nayeon",
+      message: "Bang..",
       css1: "bg-white/80 ",
       css2: "justify-start",
-      timestamp: "10:35",
+      timestamp: "07:21",
     },
     {
-      message: "Nice to meet you Joe Doe!",
+      message: "Iya?",
       css1: "bg-black/90 text-white",
       css2: "justify-end",
-      timestamp: "10:36",
+      timestamp: "07:21",
     },
     {
-      name: "Joe Doe",
-      message: "Are you subscribed yet?",
+      name: "Nayeon",
+      message: "Besok shooting gak?",
       css1: "bg-white/80 ",
       css2: "justify-start",
-      timestamp: "10:35",
+      timestamp: "08:03",
     },
     {
-      message: "Actually i will subscribe to Goose Dev!",
+      message: "Iya dong. Di cafe de casa ya. Jam 8~",
       css1: "bg-black/90 text-white",
       css2: "justify-end",
-      timestamp: "10:36",
+      timestamp: "08:04",
     },
     {
-      name: "Joe Doe",
-      message: "Have you joined the discord as well?",
+      name: "Nayeon",
+      message: "Okay siap",
       css1: "bg-white/80 ",
       css2: "justify-start",
-      timestamp: "10:35",
+      timestamp: "08:04",
     },
     {
-      message: "No but i will join.",
+      message: "Oc",
       css1: "bg-black/90 text-white",
       css2: "justify-end",
-      timestamp: "10:36",
+      timestamp: "08:04",
     },
   ];
   return (
-    <div className="hidden md:flex flex-col justify-between items-center w-full bg-[#eeedef] border-l relative  rounded-r-[2rem]  ">
+    <div className="chatBody">
       {/* Header */}
-      <div className=" flex justify-between items-center px-4  w-full h-[4.5rem] z-20 bg-[#eeedef] rounded-tr-[2rem]">
-        <div className="flex items-center">
-          <div className="flex w-[3.1rem] h-[3.1rem] bg-[#dda117] rounded-full shrink-0 ">
+      <div className="chatTop">
+        <div className="chatProfilePosition">
+          <div className="chatProfilePic">
             <img
-              src={profile3}
+              src={profil_chat}
               alt=""
-              className="object-cover rounded-full p-1"
+              className="chatProfileShape"
             />
           </div>
-          <div className="pl-2 ">
-            <p className="font-bold text-black/90">Cool Kids</p>
-            <p className="text-[14px] w-[15rem] whitespace-nowrap truncate text-gray-400 ">
-              Lorem ipsum dolor sit amet consectetur.
-            </p>
+          <div className="chatProfileAlign">
+            <p className="chatName">Nayeon</p>
+            <p className="chatLastSeen">Last seen 08:40</p>
           </div>
         </div>
 
@@ -74,22 +71,17 @@ const Chats = () => {
         </div>
       </div>
       {/* Messages */}
-      <div className="absolute top-0 w-full  h-full ">
-        <div className="absolute flex h-full w-full lg:hidden  rounded-r-[2rem]">
+      <div className="chatMessageBodyAlign">
+        <div className="chatMessageBodyPosition">
+        </div>
+        <div className="chatMessagePop">
           <img
-            src={landscape}
+            src={chat_background}
             alt=""
-            className="object-cover h-full w-full rounded-r-[3rem]"
+            className="chatBackgroundPosition"
           />
         </div>
-        <div className="hidden lg:flex absolute   h-full w-full rounded-tr-[3rem]">
-          <img
-            src={mountains}
-            alt=""
-            className="object-cover h-full w-full rounded-r-[3.1rem]"
-          />
-        </div>
-        <div className="relative z-20  pt-[4.5rem] mt-3 lg:mt-4">
+        <div className="chatMessagePopAlign">
           {messages.map((message) => (
             <Message
               name={message.name}
@@ -103,14 +95,14 @@ const Chats = () => {
       </div>
 
       {/* Input for message */}
-      <div className="relative z-20  bottom-0 bg-[#eeedef]  w-full px-4 py-2 flex items-center rounded-br-[3rem]   ">
-        <div className="">
-          <TbApps className="w-8 h-8" />
+      <div className="chatTyping">
+        <div className="input_message">
+          <TbApps className="chatTypingIconSize" />
         </div>
         <input
           type="text"
           placeholder="Type a message..."
-          className="w-full mx-4 p-2 rounded-full pl-4  "
+          className="chatTypingShape"
         />
         <div className="">
           <AiOutlineFile className="w-7 h-7" />
